374cca2ae5eb084bea377548f6a40ceef02eb570
Digbijaya Biswal <digbijaya.biswal1234@gmail.com>
2024-09-11 20:51:13 +0530

    Implemeted basic CRUD with get, post, put and delete

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let employeeData = [
+    {
+        name:"Digbijaya Biswal",
+        designation:"Software Engineer 2",
+        department:"DSE",
+        id:"2024"
+    }
+]
+
+app.get("/", function(req, res){
+    res.send(employeeData);
+})
+
+app.post("/", function(req,res){
+    if(!req.body.name ){
+        res.status(400).send('fill all required data!');
+    }else{
+        employeeData.push(req.body);
+        res.send("Data has been saved!!")
+    }
+    
+})
+
+app.put('/:id', function(req,res){
+    // console.log(req.params.id)
+    const reqID = req.params.id;
+    employeeData.map((each)=>{
+        if(each.id !== reqID){
+            res.status(404).send('Id not found'); 
+        }
+    })
+
+  employeeData.filter((each) =>{
+   if(each.id === reqID){
+    each.department = req.body.department;
+    each.designation = req.body.designation
+   }
+    })
+    // console.log(employeeData)
+    res.send(employeeData)
+        
+    }
+)
+
+
+app.delete('/:id', function(req,res){
+    // console.log(req.params.id)
+    const reqID = req.params.id;
+    employeeData.map((each)=>{
+        if(each.id !== reqID){
+            res.status(404).send('Id not found'); 
+        }
+    })
+  employeeData = employeeData.filter((each) =>{ return each.id !== reqID })
+
+  res.send("deleted")
+
+})
+app.listen(3000,()=>{
+    console.log("started");
+});
+
+// 112, 131, video
+//15 video completed 
